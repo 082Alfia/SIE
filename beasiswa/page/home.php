c5eb43d5ec242bae4294f6e1b9bfa9223af4676b
082Alfia <92343886+082Alfia@users.noreply.github.com>
2021-12-12 19:42:15 +0700

    Add files via upload

---
+++ beasiswa/page/home.php
@@ -0,0 +1,8 @@
+<body bgcolor="Pink">
+<h1><p align="center">
+<font color="maroon">
+   	Aplikasi Pendukung pemilihan siswa yang pantas mendapatkan beasiswa dengan metode SAW
+</h1> 
+</b>
+</p>
+</font>
